dde256c08a192283b9596a4638815655b8e1525a
nathanbegbie <nathanbegbie@gmail.com>
2016-08-17 12:17:59 +0200

    Completed tests for Question model's state - ensuring valid state transitions

--- core/models.py
+++ core/models.py
@@ -2,27 +2,41 @@
 from django.db import models
 
 
+class StateException(Exception):
+    """An Exception when incorrect action is called on a given state."""
+
+    def __init__(self, state):
+        """Constructor for StateException."""
+        self.state = state
+
+    def __str__(self):
+        """Return state error message indicating why exception is thrown."""
+        return ("Incorrect state change. Current state is " +
+                str(self.state))
+
+
 class Question(models.Model):
     """Question class containing challenges."""
 
-    # Question States
-    INCOMPLETE = 1
-    REVIEW_READY = 2
-    NEEDS_REWORKING = 3
-    COMPLETE = 4
-    FLAGGED = 5
+    INCOMPLETE = 0
+    REVIEW_READY = 1
+    NEEDS_REWORKING = 2
+    COMPLETE = 3
+    FLAGGED = 4
+
+    QUESTION_STATES = (
+        (INCOMPLETE, "Incomplete"),
+        (REVIEW_READY, "Ready for Review"),
+        (NEEDS_REWORKING, "Needs Reworking"),
+        (COMPLETE, "Complete"),
+        (FLAGGED, "Flagged for review")
+    )
 
     content = models.TextField()
     explanation = models.TextField()
+    # WARNING: DO NOT CHANGE STATE DIRECTLY, USE STATE CHANGE METHODS
     state = models.PositiveIntegerField("State",
-                                        choices=(
-                                            (INCOMPLETE, "Incomplete"),
-                                            (REVIEW_READY, "Ready for Review"),
-                                            (NEEDS_REWORKING,
-                                                "Needs Reworking"),
-                                            (COMPLETE, "Complete"),
-                                            (FLAGGED, "Flagged for Review")
-                                        ),
+                                        choices=QUESTION_STATES,
                                         default=INCOMPLETE)
 
     def change_to_review_ready(self):
@@ -32,12 +46,11 @@ class Question(models.Model):
         "Incomplete"
         or "Needs Reworking".
         """
-        if (self.state is not self.INCOMPLETE or
-                self.state is not self.NEEDS_REWORKING):
-            raise ("Incorrect state change. Current state is " +
-                   str(self.state))
-        else:
+        if (self.state is self.INCOMPLETE or
+                self.state is self.NEEDS_REWORKING):
             self.state = self.REVIEW_READY
+        else:
+            raise StateException(self.state)
 
     def change_to_needs_reworking(self):
         """Change state of question.
@@ -45,11 +58,10 @@ class Question(models.Model):
         Changes state to "Needs Reworking", only if in the state of
         "Ready for Review".
         """
-        if self.state is not self.REVIEW_READY:
-            raise ("Incorrect state change. Current state is " +
-                   str(self.state))
-        else:
+        if self.state is self.REVIEW_READY:
             self.state = self.NEEDS_REWORKING
+        else:
+            raise StateException(self.state)
 
     def change_to_complete(self):
         """Change state of question.
@@ -57,23 +69,21 @@ class Question(models.Model):
         Changes state to "Complete", only if in the state of "Ready for Review"
         or "Flagged for Review".
         """
-        if (self.state is not self.REVIEW_READY or
-                self.state is not self.FLAGGED):
-            raise ("Incorrect state change. Current state is " +
-                   str(self.state))
-        else:
+        if (self.state is self.REVIEW_READY or
+                self.state is self.FLAGGED):
             self.state = self.COMPLETE
+        else:
+            raise StateException(self.state)
 
     def change_to_flagged(self):
         """Change state of question.
 
         Changes state to "Flagged", only if in the state of "Complete".
         """
-        if self.state is not self.COMPLETE:
-            raise ("Incorrect state change. Current state is " +
-                   str(self.state))
-        else:
+        if self.state is self.COMPLETE:
             self.state = self.FLAGGED
+        else:
+            raise StateException(self.state)
 
 
 class Answer(models.Model):
