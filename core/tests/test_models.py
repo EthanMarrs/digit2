3708b14c31ade1d9d5c58a02c9bc64f01ab36db0
nathanbegbie <nathanbegbie@gmail.com>
2016-08-17 08:17:49 +0200

    Created simple tests for raising exceptions for Question class

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,13 +1,45 @@
+"""test_models.py: runs tests on the models for digit."""
+import pytest
 from core.models import Question, Answer
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
 
 class TestQuestion(TestCase):
-    def setUp(self):
-        question = Question(content='What is the airspeed velocity of an unladen swallow?',
-                            explanation='Invert the midpoint Strouhal number (which is 0.3). This means that the \
-                            airspeed about 3 times the product of the frequency and the amplitude.')
-        answer = Answer(content='11 meters per second.', question=question)
+    """Test the Question Model."""
 
-    def test_test(self):
-        assert(True)
+    def setUp(self):
+        """Create questions for testing."""
+        question1 = Question(content='what is 1 + 1?',
+                             explanation='This is an addition question')
+        question1.save()
+        answer1 = Answer(content='2', question=question1)
+        answer1.save()
+
+    def test_question_deafult_state(self):
+        """Confirm that default state is Incomplete."""
+        question1 = Question.objects.all()[0]
+        assert(question1.state == 1)
+
+    def test_question_answer_relation(self):
+        """Confirm that question can only have one answer."""
+        question1 = Question.objects.all()[0]
+        with pytest.raises(IntegrityError) as exception_info:
+            another_answer = Answer(content='3', question=question1)
+            another_answer.save()
+        assert(exception_info.value.__str__() ==
+               "UNIQUE constraint failed: core_answer.question_id")
+
+    def test_question_state_from_incomplete_to_review_ready(self):
+        """Check that question state.
+
+        Confirm that state can only go from 'incomplete' to
+        'ready for review'.
+        """
+        question1 = Question.objects.all()[0]
+        with pytest.raises(Exception) as exception_info:
+            question1.change_to_needs_reworking()
+        for item in dir(exception_info):
+            print(item)
+        assert(exception_info.value.__str__() ==
+               "Incorrect state change. Current state is 1")
